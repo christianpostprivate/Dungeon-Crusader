db2e344fa222ecc94a844cabb1f00de9744fae15
MattR0se <37417239+MattR0se@users.noreply.github.com>
2018-07-28 15:40:02 +0200

    Add files via upload

--- rooms.py
+++ rooms.py
@@ -42,37 +42,65 @@ class Room():
         for i in range(self.h):
             self.layout.append([])
             self.tiles.append([])
-            if i == 0 :
+            if i <= 1:
+                # upper walls
                 for j in range(self.w):
                     self.layout[i].append(1)
                     
-                    if j == 0:
-                        self.tiles[i].append(12)
-                    elif j == self.w - 1:
-                        self.tiles[i].append(13)
-                    else:
-                        self.tiles[i].append(28)
-            elif i == self.h - 1:
+                    if i == 1:
+                    
+                        if j == 0:
+                            self.tiles[i].append(19)
+                        elif j == 1:
+                            self.tiles[i].append(12)
+                        elif j == self.w - 1:
+                            self.tiles[i].append(19)
+                        elif j == self.w - 2:
+                            self.tiles[i].append(13)
+                        else:
+                            self.tiles[i].append(28)
+                    elif i == 0:
+                        self.tiles[i].append(19)
+                        
+            elif i >= self.h - 2:
+                # lower walls
                 for j in range(self.w):
                     self.layout[i].append(1)
                     
-                    if j == 0:
-                        self.tiles[i].append(21)
-                    elif j == self.w - 1:
-                        self.tiles[i].append(22)
-                    else:
-                        self.tiles[i].append(10)
+                    if i == self.h - 2:
+                        if j == 0:
+                            self.tiles[i].append(19)
+                        elif j == 1:    
+                            self.tiles[i].append(21)
+                        elif j == self.w - 2:
+                            self.tiles[i].append(22)
+                        elif j == self.w - 1:
+                            self.tiles[i].append(19)
+                        else:
+                            self.tiles[i].append(10)
+                            
+                    elif i == self.h - 1:
+                        self.tiles[i].append(19)
+                            
             else:
                 for j in range(self.w):
-                    if j == 0:
+                    if j <= 1:
+                        # left wall
                         self.layout[i].append(1)
                         
-                        self.tiles[i].append(20)
+                        if j == 1:
+                            self.tiles[i].append(20)
+                        else:    
+                            self.tiles[i].append(19)
                         
-                    elif j == self.w - 1:    
+                    elif j >= self.w - 2:
+                        # right wall
                         self.layout[i].append(1)
                         
-                        self.tiles[i].append(18)
+                        if j == self.w - 2:
+                            self.tiles[i].append(18)
+                        else:
+                            self.tiles[i].append(19)
                     else:
                         self.layout[i].append(0)
                         self.tiles[i].append(0)
@@ -80,47 +108,51 @@ class Room():
         # north
         if 'N' in self.doors:
             self.layout[0][door_w] = 0
+            self.layout[1][door_w] = 0
             # 35, 34, 33
-            self.tiles[0][door_w + 1] = 35
-            self.tiles[0][door_w] = 34
-            self.tiles[0][door_w - 1] = 33
+            self.tiles[1][door_w + 1] = 35
+            self.tiles[1][door_w] = 34
+            self.tiles[1][door_w - 1] = 33
             
         # south
         if 'S' in self.doors:
+            self.layout[self.h - 2][door_w] = 0
             self.layout[self.h - 1][door_w] = 0
             
-            self.tiles[self.h - 1][door_w + 1] = 32
-            self.tiles[self.h - 1][door_w] = 31
-            self.tiles[self.h - 1][door_w - 1] = 30
+            self.tiles[self.h - 2][door_w + 1] = 32
+            self.tiles[self.h - 2][door_w] = 31
+            self.tiles[self.h - 2][door_w - 1] = 30
         
         # west
         if 'W' in self.doors:
             self.layout[door_h][0] = 0
+            self.layout[door_h][1] = 0
             
-            self.tiles[door_h + 1][0] = 24
-            self.tiles[door_h][0] = 15
-            self.tiles[door_h - 1][0] = 6
+            self.tiles[door_h + 1][1] = 24
+            self.tiles[door_h][1] = 15
+            self.tiles[door_h - 1][1] = 6
         
         # east
         if 'E' in self.doors:
+            self.layout[door_h][self.w - 2] = 0
             self.layout[door_h][self.w - 1] = 0
             
-            self.tiles[door_h + 1][self.w - 1] = 23
-            self.tiles[door_h][self.w - 1] = 14
-            self.tiles[door_h - 1][self.w - 1] = 5
+            self.tiles[door_h + 1][self.w - 2] = 23
+            self.tiles[door_h][self.w - 2] = 14
+            self.tiles[door_h - 1][self.w - 2] = 5
         
         
     def buildInterior(self):
         # in the room
-        w = self.w - 1
-        h = self.h - 1
+        w = self.w - 2
+        h = self.h - 2
         
         door_w = self.w // 2
         door_h = self.h // 2
         # floor tile index
         floor = 4
-        for i in range(1, h):
-            for j in range(1, w):
+        for i in range(2, h):
+            for j in range(2, w):
                 if randint(0, 100) <= 10 and (
                         i != door_h and j != door_w): 
                     self.layout[i][j] = 1    
@@ -133,7 +165,6 @@ class Room():
 
 class Dungeon():
     def __init__(self, game, size):
-        start = datetime.now()
         self.size = vec(size)
         self.game = game
         # variables for animation
@@ -157,23 +188,18 @@ class Dungeon():
         
         self.tileset = choice(self.game.tileset_names)
         
-        self.seed = randint(100000, 999999)
+        #self.seed = randint(100000, 999999)
         #self.seed = 123456
         
-        self.build()
+        #self.create(None)
         
-        dt = datetime.now() - start
-        ms = dt.seconds * 1000 + dt.microseconds / 1000.0
-        print('Dungeon built in {} ms'.format(round(ms, 1)))
-
-    
-    def saveSelf(self):       
+        
+    def saveSelf(self):   
         self.saveGame.data = {**self.saveGame.data,
                               'size': (self.size.x, self.size.y),
                               'room_index': self.room_index,
                               'seed': self.seed
-                              }
-       
+                              }     
         self.saveGame.save()
        
         
@@ -185,16 +211,35 @@ class Dungeon():
             self.room_index = self.saveGame.data['room_index']
             self.seed = self.saveGame.data['seed']
             
-            self.build()
+            self.create(self.seed)
 
         except:
             pass
-
+       
         
-    def build(self):   
+    def create(self, rng_seed): 
+        start = datetime.now()
+        
+        if rng_seed != None:
+            self.seed = rng_seed
+        else:
+            self.seed = randint(1000000, 9999999)
+        
+        print('Dungeon seed: ', self.seed)
+        
+        self.build(rng_seed)
+
+        self.closeDoors()
+        self.floodFill()
+        
+        dt = datetime.now() - start
+        ms = dt.seconds * 1000 + dt.microseconds / 1000.0
+        print('Dungeon built in {} ms'.format(round(ms, 1)))
+        
+        
+    def build(self, rng_seed):   
         # set seed for randomisation
-        seed(a=self.seed)
-        print(self.seed)
+        seed(a=rng_seed)
         
         while self.done == False:
             self.done = True
@@ -272,9 +317,6 @@ class Dungeon():
                                 self.rooms[i + 1][j] = Room(self.game, rng)
                                 
                             self.done = False
-                            
-            self.closeDoors()
-            self.floodFill()
 
     
     def closeDoors(self):
@@ -343,12 +385,13 @@ class Dungeon():
         # blit a mini-map image onto the screen
         
         # room image size
-        size = (int(3.5 * st.GLOBAL_SCALE), int(3.5 * st.GLOBAL_SCALE / 2))
+        #size = (int(3.5 * st.GLOBAL_SCALE), int(3.5 * st.GLOBAL_SCALE / 2))
+        size = (6, 4)
         
         # mini map size
-        w = self.size.x * size[0]
-        h = st.GUI_HEIGHT - 2 * st.GUI_MARGIN
-        margin = st.GUI_MARGIN
+        w = 59
+        h = 39
+        margin = 4 * st.GLOBAL_SCALE
 
         self.map_img = pg.Surface((w, h), flags=pg.SRCALPHA)
         self.map_img.fill(st.BLACK)
@@ -356,33 +399,31 @@ class Dungeon():
         for i in range(len(self.rooms)):
             for j in range(len(self.rooms[i])):
                 room = self.rooms[i][j]
-                pos = (j * (w / self.size.x), i * (h / self.size.y))
+                #pos = (j * (w / self.size.x) - 1, i  * (h / self.size.y) - 1)
+                pos = (j * size[0] - 1, i * size[1] - 1)
                 if room and room.visited:
-                    self.map_img.blit(pg.transform.scale(room.image,
-                                      size), pos)
+                    self.map_img.blit(room.image, pos)
                     if room.type == 'start':
                         # draw a square representing the starting room
-                        self.map_img.blit(pg.transform.scale(
-                                self.game.room_images[12], size), pos)
+                        self.map_img.blit(self.game.room_images[17], pos)
                 else:
-                    self.map_img.blit(pg.transform.scale(
-                            self.game.room_images[17], size), pos)
-         
+                    self.map_img.blit(self.game.room_images[0], pos)
+
         # animated red square representing the player
         now = pg.time.get_ticks()
-        pos2 = (self.room_index[1] * (w / self.size.x), 
-                self.room_index[0] * (h / self.size.y))
-        player_imgs = [pg.transform.scale(self.game.room_images[11], size),
-                       pg.transform.scale(self.game.room_images[17], size)]
+        pos2 = (self.room_index[1] * size[0] - 1, 
+                self.room_index[0] * size[1] - 1)
+        player_imgs = [self.game.room_images[18], self.game.room_images[19]]
         
         if now - self.last_update > 500:
                 self.last_update = now
                 # change the image
                 self.current_frame = (self.current_frame + 1) % len(player_imgs)
         self.map_img.blit(player_imgs[self.current_frame], pos2)
-                
-        self.game.inventory.image.blit(self.map_img, 
-                                       (st.WIDTH - w - margin, 
+        
+        scaled = (w * st.GLOBAL_SCALE, h * st.GLOBAL_SCALE)
+        self.game.inventory.image.blit(pg.transform.scale(self.map_img, scaled), 
+                                       (st.WIDTH - scaled[0] - margin, 
                                         st.HEIGHT - st.GUI_HEIGHT + margin))
         
         
